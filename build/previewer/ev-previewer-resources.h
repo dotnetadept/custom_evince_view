fb9cce21e5b3acd4f9ae8f37bc9f94ee47779e84
vladimir <dotnetadept@gmail.com>
2021-09-19 01:43:56 +0300

    files added

---
+++ build/previewer/ev-previewer-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_ev_previewer_H__
+#define __RESOURCE_ev_previewer_H__
+
+#include <gio/gio.h>
+
+extern GResource *ev_previewer_get_resource (void);
+#endif
---
+++ build/shell/ev-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_ev_H__
+#define __RESOURCE_ev_H__
+
+#include <gio/gio.h>
+
+extern GResource *ev_get_resource (void);
+#endif
